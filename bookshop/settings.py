c231d9a89a4c6dff303c39d210d259b4e6f214ac
Md Tanzil <mtanzil1999@gmail.com>
2024-07-15 15:53:07 +0600

    add env and debug tool

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -13,18 +13,24 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from oscar.defaults import *
 from pathlib import Path
 import os
-
+import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rga$1h)yfy+2k%kooitxmxmr&g2%*plb%-*!(t17$g=ues_dey"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
